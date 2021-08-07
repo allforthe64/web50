5c7369c0ee29862cae4a3ecfdc0e002f688907b3
will <williamhrainey@gmail.com>
2021-08-07 11:33:42 -0700

    finished search function

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -10,6 +10,9 @@ import os
 pages = []
 entries = "/Users/linds/Desktop/will/web50/web50/wiki/entries"
 
+#initalize list to hold search entries
+relatedEntries = []
+
 def index(request):
 
     #clear out list
@@ -32,32 +35,68 @@ def index(request):
         if query in pages:
             return redirect(f"wiki/{query}")
         else:
+            #clear related entries list
+            relatedEntries.clear()
 
             #iterate over each entry in the page and check if substring in entry
-            print(pages)
             for page in pages:
                 if query in page:
-                    print(page)
-                    print("Found!")
+                    relatedEntries.append(page)
 
-            return render(request, "encyclopedia/index.html")
+            return render(request, "encyclopedia/related.html",{
+                "entries": relatedEntries
+            })
 
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request, targetPage):
-    #search for .md file and get its contents
-    pageContent = util.get_entry(targetPage)
+    
+    if request.method == "GET":
+        #search for .md file and get its contents
+        pageContent = util.get_entry(targetPage)
 
-    #check to see if a result was found
-    if pageContent == None:
-        return render(request, "encyclopedia/error.html")
-    #if result was found render the page
+        #check to see if a result was found
+        if pageContent == None:
+            return render(request, "encyclopedia/error.html")
+        #if result was found render the page
+        else:
+            return render(request, f"encyclopedia/Entry.html", {
+                "contents": pageContent,
+                "pageTitle": targetPage
+            })
     else:
-        return render(request, f"encyclopedia/Entry.html", {
-            "contents": pageContent,
-            "pageTitle": targetPage
-        })
+        #clear out list
+        pages.clear()
+
+        for entry in os.listdir(entries):
+            
+            #split off the ".md" file tag
+            entry = str(entry)
+            add = entry.split(".md")
+
+            #append file name to pages list
+            pages.append(add[0])
+
+
+        #search list of entries for page
+        if request.method == "POST":
+            query = request.POST.get("q")
+            
+            if query in pages:
+                return redirect(f"wiki/{query}")
+            else:
+                #clear related entries list
+                relatedEntries.clear()
+
+                #iterate over each entry in the page and check if substring in entry
+                for page in pages:
+                    if query in page:
+                        relatedEntries.append(page)
+
+                return render(request, "encyclopedia/relatedPage.html",{
+                    "entries": relatedEntries
+                })
 
 
