1ae9a20b0602e44b5f4f3e66f92bb634e34ee9d2
will <williamhrainey@gmail.com>
2021-08-09 16:18:04 -0700

    added new page route

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:targetPage>", views.entry, name="entry"),
     path("search/<str:targetPage>", views.entry, name="search"),
+    path("/new", views.new, name="new")
 ]
