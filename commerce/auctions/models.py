ad9b8268f38da388853057b0c560d0fa2a0fa94b
allforthe64 <williamhrainey@gmail.com>
2021-08-13 11:13:06 -0700

    added listings class

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class listing(models.Model):
+    title = models.CharField(max_length = 64, primary_key = True, help_text = "Enter listing's title")
+    descrpt = models.TextField(help_text = "Enter item's description")
+    beginningBid = models.IntegerField(help_text = "Enter minimum price of item")
+    img = models.URLField(max_length = 200, blank = True, help_text = "*Optional* Enter image URL")
+    category = models.CharField(max_length = 64, blank = True, help_text = "*Optional* Enter listing category")
