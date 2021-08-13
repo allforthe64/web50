8e0638a3ebb6546dd5b6669f33722cce49e9716d
allforthe64 <williamhrainey@gmail.com>
2021-08-13 12:56:07 -0700

    added 'active listings' functionality

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -6,9 +6,11 @@ class User(AbstractUser):
     pass
 
 
-class listing(models.Model):
-    title = models.CharField(max_length = 64, primary_key = True, help_text = "Enter listing's title")
-    descrpt = models.TextField(help_text = "Enter item's description")
-    beginningBid = models.IntegerField(help_text = "Enter minimum price of item")
+class Listing(models.Model):
+    id = models.AutoField(primary_key = True, default = None)
+    title = models.CharField(max_length = 64, help_text = "Enter listing's title")
+    description = models.TextField(help_text = "Enter item's description")
+    beginningBid = models.FloatField(help_text = "Enter minimum price of item")
     img = models.URLField(max_length = 200, blank = True, help_text = "*Optional* Enter image URL")
     category = models.CharField(max_length = 64, blank = True, help_text = "*Optional* Enter listing category")
+    active = models.BooleanField(default = True)
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -3,13 +3,23 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-from .models import User
+from .models import User, Listing
 
-
+@login_required(login_url='/login')
 def index(request):
-    return render(request, "auctions/index.html")
 
+    listings = Listing.objects.all().filter(active = True)
+    links = []
+
+    for listing in listings:
+        links.append(listing.title)
+
+    print(links)
+    return render(request, "auctions/index.html", {
+        "listings": links,
+    })
 
 def login_view(request):
     if request.method == "POST":
@@ -61,3 +71,8 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def listing(request):
+    pass;   
+    
+    
