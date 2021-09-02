f91e80bdc081e57d9f5ecbc23f7f884f2328951a
allforthe64 <williamhrainey@gmail.com>
2021-09-02 14:52:45 -0700

    finished the project

--- projects/2020/x/mail/mail/static/mail/inbox.js
+++ projects/2020/x/mail/mail/static/mail/inbox.js
@@ -33,7 +33,7 @@ function load_mailbox(mailbox) {
   document.querySelector('#email-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3><br>`;
 
   // Get request
   fetch(`/emails/${mailbox}`)
@@ -46,13 +46,13 @@ function load_mailbox(mailbox) {
         document.querySelector('#emails-view').innerHTML = '<p style = "font-size: large; font-weight: bold;">Nothing to see here :(</p>';
       }
       else{
-        email_view = document.querySelector('#emails-view');
+        emails_view = document.querySelector('#emails-view');
         for (email in emails) {
 
           //establish variables
           var mail = document.createElement("div");
           var sender = document.createElement("h5");
-          var sub = document.createElement('p');
+          var subject = document.createElement('p');
           var time = document.createElement('p');
           var id = document.createElement('p');
 
@@ -63,23 +63,24 @@ function load_mailbox(mailbox) {
           // if message has no subject, set subject to equal no subject and make the color red
           sender.innerHTML = emails[email]['sender']
           if (emails[email]['ubject'] == '') {
-            sub.innerHTML = 'No Subject';
-            sub.style.color = 'red';
+            subject.innerHTML = 'No Subject';
+            subject.style.color = 'red';
           }
           else {
-            sub.innerHTML = emails[email]['subject'];
+            subject.innerHTML = emails[email]['subject'];
           }
           time.innerHTML = emails[email]['timestamp'];
           
           // set border styling for the div
           mail.style.borderStyle = 'solid';
           mail.style.borderColor = 'black';
-          mail.style.borderWidth = '0.1rm';
-          mail.style.marginBottom = '0.2rem';
+          mail.style.borderWidth = '1px';
+          mail.style.marginBottom = '10px';
           
           // if an email has been read or not
           if (emails[email].read == true) {
             mail.style.backgroundColor = "lightgray"
+            mail.style.borderStyle = 'dashed'
           }
           else {
             mail.style.backgroundColor = 'white';
@@ -91,28 +92,28 @@ function load_mailbox(mailbox) {
 
           // style sender
           sender.style.display = 'inline-block';
-          sender.style.margin = '1rem';
+          sender.style.margin = '15px';
           
           // style sub
-          sub.style.display = 'inline-block';
-          sub.style.margin = '1rem';
+          subject.style.display = 'inline-block';
+          subject.style.marginLeft = '5px';
 
           //style time
           time.style.display = 'inline-block';
-          time.style.margin = '1rem';
+          time.style.margin = '15px';
           time.style.float = 'right';
-          time.style.color = 'blue';
+          time.style.color = 'black';
 
           // append mail div to email view and append elements into mail div
-          email_view.appendChild(mail);
+          emails_view.appendChild(mail);
           mail.appendChild(sender);
-          mail.appendChild(sub);
+          mail.appendChild(subject);
           mail.appendChild(time);
           mail.appendChild(id);
 
           // what to do if something is clicked
           mail.addEventListener('click', () => load_email());
-          sub.addEventListener('click', () => load_email());
+          subject.addEventListener('click', () => load_email());
           time.addEventListener('click', () => load_email());
           sender.addEventListener('click', () => load_email());
         }
@@ -122,71 +123,75 @@ function load_mailbox(mailbox) {
 
 }
 
-//shows the email you clicked on from mailbox
+// shows the email you clicked on from mailbox
 function load_email() {
+  
+  //shutoff any other event listeners that may have been called
   event.stopImmediatePropagation();
-  //keeping only the email-view and hiding the rest
+
+  // hiding everything but the email view
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
   mail_view = document.querySelector('#email-view');
   mail_view.style.display = 'block';
 
-  var tar = event.target;
-  console.log(tar.children);
+  var target = event.target;
+  console.log(target.children);
 
-  //if the element clicked on is not div, we choose its parent (which is the div)
-  if (!(tar.tagName == 'DIV')) {
-    tar = tar.parentElement;
+  // make sure the parent element is selected
+  if (!(target.tagName == 'DIV')) {
+    target = target.parentElement;
   }
 
-  //we take the div's children elements and extract the id (we know that it is the fourth (third index) element as we checked using console.log)
-  var c = tar.children;
-  var id = c[3].innerHTML;
+  // extract id out of child elements
+  var children = target.children;
+  var id = children[3].innerHTML;
 
-  //clearing old content
+  // clearing old content
   mail_view.innerHTML = '';
 
-  //we make a GET request to get everything we need about the email
+  // Get request
   fetch(`/emails/${id}`)
     .then(response => response.json())
     .then(email => {
 
-      //creating a div for each email in mailbox in which we will include sender, timestamp and subject 
+      // creating divs for each email in the sent box 
       var div = document.createElement('div');
 
-      //adding classes to that div, bootstrap sheeyit
+      // add classes to the div
       div.classList.add('container');
-      div.classList.add('jumbotron');
 
-      //creating the needed elements for subject, sender, etc.
-      var sub = document.createElement('h3');
-      sub.innerText = email['subject'];
-      var sender = document.createElement('h5');
+      // creating elements
+      var sender = document.createElement('h3');
       sender.innerText = `From: ${email['sender']}`;
+      var subject = document.createElement('h5');
+      subject.innerText = `Subject: ${email['subject']}`;
       var body = document.createElement('p');
       body.innerText = email['body'];
       var time = document.createElement('p');
       time.innerText = email['timestamp'];
 
-      //styling for timestamp
-      time.style.color = 'blue';
+      // styling for timestamp
+      time.style.color = 'black';
 
-      //styling for body
-      body.style.padding = '2rem';
-      body.style.backgroundColor = 'lightgray';
+      // styling for body text
+      body.style.padding = '20px';
+      body.style.borderStyle = 'solid';
+      body.style.borderWidth = '1px';
+      body.style.borderColor = 'black';
 
-      //adding the elements to the div we created
-      div.appendChild(sub);
+      // adding the elements to the smaller div
       div.appendChild(sender);
+      div.appendChild(subject);
       div.appendChild(time);
 
-      //adding the div and body to the main div
+      // adding elements to the main div
       mail_view.appendChild(div);
       mail_view.appendChild(body);
 
-      //making the read attribute true in case it was false
+      // making sure read variable is set to true
       if (email['read'] == false) {
-        //making the read attribute true
+        // setting read atribute
         fetch(`/emails/${id}`, {
           method: 'PUT',
           body: JSON.stringify({
@@ -195,54 +200,55 @@ function load_email() {
         })
       }
 
-      //Adding buttons from here on
+
       var archive = email['archived'];
 
-      //archive toggle and reply button
+      // archive and reply buttons
       var btn = document.createElement('button');
       var reply = document.createElement('button');
 
-      //setting buttons' inner text
-      //if the email is already archived
+      reply.style.marginLeft = "20px";
+
+      // if the email is already archived
       if (archive) {
-        btn.innerText = 'Unarchive';
+        btn.innerText = 'Remove From Archive';
       }
       else {
         btn.innerText = 'Archive';
       }
       reply.innerText = 'Reply';
 
-      //bootstrap sheeyit
+      // add bootstrap classes
       btn.classList.add('btn-primary');
       btn.classList.add('btn');
       reply.classList.add('btn-primary');
       reply.classList.add('btn');
 
-      //adding event listeners to both buttons
+      // adding event listeners to both buttons
 
-      //first the archive toggle
+      // archive toggle
       btn.addEventListener('click', () => {
-        //got this from the API documentation
         fetch(`/emails/${id}`, {
           method: 'PUT',
           body: JSON.stringify({
             archived: !archive
           })
         });
-        //loading inbox after done toggling archive property
+        // loading inbox after toggling archive property
         load_mailbox('inbox');
       });
 
-      //reply button function
+      // reply button function
       reply.addEventListener('click', () => {
 
-        //opens the compose mail section and hides all the others
+        // opens the compose mail section and hides all the others
         compose_email();
 
-        //setting default values as specified
+        // set default values
         document.querySelector('#compose-recipients').value = email['sender'];
-        document.querySelector('#compose-body').value = `On ${email['timestamp']}, ${email['sender']} wrote: ${email['body']}`;
-        //checking for subject
+        document.querySelector('#compose-body').value = `${email['sender']} wrote: ${email['body']}`;
+
+        // subject checking
         if (email['subject'].search('Re:')) {
           document.querySelector('#compose-subject').value = email['subject'];
         }
@@ -251,7 +257,7 @@ function load_email() {
         }
       });
 
-      //adding the buttons to our HTML
+      // adding the buttons to page
       mail_view.appendChild(btn);
       mail_view.appendChild(reply);
     });
