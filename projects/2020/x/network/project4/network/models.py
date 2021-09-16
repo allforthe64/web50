69b81bf1b56bbb18d11081456a55c39389c53f87
allforthe64 <williamhrainey@gmail.com>
2021-09-16 11:58:55 -0700

    fixed import error

--- projects/2020/x/network/project4/network/models.py
+++ projects/2020/x/network/project4/network/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
 
 class Post(models.Model):
     post_id = models.AutoField(primary_key=True, default=None),
-    poster = models.ForeignKey(User, on_delte=models.CASCADE, related_name="user"),
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user"),
     date = models.DateField(default=None),
     time = models.TimeField(auto_now_add=True, default=None),
     content = models.TextField(default=None)
@@ -16,3 +16,4 @@ class Post(models.Model):
         return {
             "likes":self.likes
         }
+
