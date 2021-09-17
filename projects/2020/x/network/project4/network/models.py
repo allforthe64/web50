99076cd563e58dbd18f26264d65eb3c6a0146450
allforthe64 <williamhrainey@gmail.com>
2021-09-17 14:37:04 -0700

    made a new entry class in models.py

--- projects/2020/x/network/project4/network/models.py
+++ projects/2020/x/network/project4/network/models.py
@@ -5,3 +5,17 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Entry(models.Model):
+    post_id = models.AutoField(primary_key=True, default=None)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poster")
+    content = models.TextField(default=None)
+    timestamp = model.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+
+    def serialize(self):
+        return {
+            "id": self.post_id,
+            "poster": self.poster,
+            "content": self.content,
+            "timestamp": self.timestamp.strftime("%b %d %Y, %I:%M %p")
+        }
