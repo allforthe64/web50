0c785ede751b98542146d132196c708fd7b48f89
allforthe64 <williamhrainey@gmail.com>
2021-09-29 21:02:54 -0700

    Figured out the problem

--- projects/2020/x/network/project4/network/static/network/helpers.js
+++ projects/2020/x/network/project4/network/static/network/helpers.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     alert("Loaded!")
 
     //add event listeners
-    document.querySelector('#like').addEventListener('click', function() {
-        id = document.querySelector('#like').value;
+    document.querySelector('.like').addEventListener('click', function() {
+        id = document.querySelector('.like').value;
         alert(id)
     });
 
