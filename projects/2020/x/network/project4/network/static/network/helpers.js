d198fa4de6afdcad78af8a1a3535c0318e72b04d
allforthe64 <williamhrainey@gmail.com>
2021-09-30 21:26:45 -0700

    dislike function almost complete

--- projects/2020/x/network/project4/network/static/network/helpers.js
+++ projects/2020/x/network/project4/network/static/network/helpers.js
@@ -3,26 +3,60 @@ document.addEventListener('DOMContentLoaded', function() {
     //add event listeners
     document.querySelectorAll('#like').forEach(item => {
         item.addEventListener('click', event => {
-            like(item.value);
-            
-            var target = event.target;
 
-            // select the parent div
-            if (!(target.tagName == 'DIV')) {
-                target = target.parentElement
-            }
+            if (item.backgroundColor = "Blue") {
 
-            // get the parent div's children 
-            var children = target.children;
-
-            //change the inner html
-            fetch(`/like/${item.value}`)
-                .then(response => response.json())
-                .then(entry => {
-                    children[1].innerHTML = `${entry["likes"] + 1} Likes`;
-                })
+                like(item.value);
                 
+                var target = event.target;
+
+                // select the parent div
+                if (!(target.tagName == 'DIV')) {
+                    target = target.parentElement
+                }
+
+                // get the parent div's children 
+                var children = target.children;
+
+                //change the inner html
+                fetch(`/like/${item.value}`)
+                    .then(response => response.json())
+                    .then(entry => {
+                        children[1].innerHTML = `${entry["likes"] + 1} Likes`;
+                    })
+                    
+                item.innerHTML = 'Unlike';
+                item.style.width = "75px";
+                item.style.backgroundColor = "Grey";
+            }
+            
+            if (item.backgroundColor = "Grey") {
+
+                alert("dislike activated");
+                
+                dislike(item.value);
+
+                var target = event.target;
+
+                if (!(target.tagName == 'DIV')) {
+                    target = target.parentElement;
+                }
+
+                var children = target.children;
+
+                //chagne the inner html
+                fetch(`/like/${item.value}`)
+                    .then(response => response.json())
+                    .then(entry => {
+                        children[1].innerHTML = `${entry["likes"] - 1} Likes`;
+                    })
+
+                item.innerHTML = 'Like';
+                item.style.width = "60px";
+                item.style.backgroundColor = "Blue";
+            }
         })
+        
     })
 
 })
