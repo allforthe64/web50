c0630e7cf419832ec8e1797961ed90ebbde77358
allforthe64 <williamhrainey@gmail.com>
2021-10-01 16:12:13 -0700

    getting values properly

--- projects/2020/x/network/project4/network/static/network/posts.js
+++ projects/2020/x/network/project4/network/static/network/posts.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     alert("This script is working!")
-    var currentUser = document.querySelector('#currentUser');
-    var profileViewing = document.querySelector('#username');
+    var currentUser = document.querySelector('#currentUser').innerHTML;
+    var profileViewing = document.querySelector('#username').innerHTML;
 
     console.log(currentUser);
     console.log(profileViewing);
